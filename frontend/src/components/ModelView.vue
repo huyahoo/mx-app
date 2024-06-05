5aee325a615d75811822b12a7043056476e99eb5
HUY <takasugi.tut@gmail.com>
2024-06-05 16:27:27 +0200

    update

--- frontend/src/components/ModelView.vue
+++ frontend/src/components/ModelView.vue
@@ -10,14 +10,29 @@
           </select>
         </div>
         <div class="model-viewer-container">
-          <model-viewer 
+          <!-- <model-viewer 
             v-if="modelSrc" 
             :src="modelSrc" 
             alt="3D Model" 
             auto-rotate 
             camera-controls 
             style="width: 100%; height: 300px;">
+          </model-viewer> -->
+
+          <!-- <model-viewer style="width: 100%; height: 300px;" id = "glb" crossorigin="anonymous" camera-controls ></model-viewer> -->
+
+          <model-viewer src="wolvic_3d_model.glb" ar ar-modes="webxr scene-viewer quick-look" camera-controls tone-mapping="neutral" poster="poster.webp" shadow-intensity="1">
+            <div class="progress-bar hide" slot="progress-bar">
+                <div class="update-bar"></div>
+            </div>
+            <!-- <button slot="ar-button" id="ar-button">
+                View in your space
+            </button> -->
+            <!-- <div id="ar-prompt">
+                <img src="https://modelviewer.dev/shared-assets/icons/hand.png">
+            </div> -->
           </model-viewer>
+
           <!-- <model-viewer camera-controls touch-action="pan-y" alt="A 3D model of a sphere" src="./2CylinderEngine.glb">
           </model-viewer> -->
         </div>
@@ -27,6 +42,8 @@
     </div>
   </div>
 </template>
+
+<!-- <script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.5.0/model-viewer.min.js"></script> -->
 
 <script>
 import axios from 'axios';
@@ -63,10 +80,15 @@ export default {
         console.error('Error loading objects:', error);
       }
     },
-    loadModel() {
+    async loadModel() {
       if (this.selectedObject) {
         // this.modelSrc = `https://your-s3-bucket-url/${this.selectedObject}`;
-        this.modelSrc = '/Users/huyphung/Repositories/personal/mx-app/frontend/public/wolvic_3d_model.glb';
+        // this.modelSrc = '/Users/huyphung/Repositories/personal/mx-app/wolvic_3d_model.glb';
+        const file = 'wolvic_3d_model.glb';
+        const response = await axios.get(`http://localhost:5000/get-image?filename=${file}`);
+        console.log(response.data)
+        document.getElementById('glb').src = response.data;
+        // this.modelSrc = response.data;
       }
     },
     downloadModel() {
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import "https://ajax.googleapis.com/ajax/libs/model-viewer/3.5.0/model-viewer.min.js";
+import '@fortawesome/fontawesome-free/css/all.css';
 
 const app = createApp(App)
 
