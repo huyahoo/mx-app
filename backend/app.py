247796c679aef040a7af34cac242a31a6db24119
HUY <takasugi.tut@gmail.com>
2024-06-10 18:15:24 +0200

    update port

--- backend/app.py
+++ backend/app.py
@@ -87,4 +87,4 @@ def serve(path):
         return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
