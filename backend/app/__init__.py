04c0c6671891b086835b877aa00dd50d4d90a378
HUY <takasugi.tut@gmail.com>
2024-06-10 17:18:44 +0200

    modify init py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,10 @@
-from flask import Flask
-from .routes import upload_bp
+# from flask import Flask
+# from .routes import upload_bp
 
-def create_app():
-    app = Flask(__name__)
+# def create_app():
+#     app = Flask(__name__)
 
-    # Register the Blueprint
-    app.register_blueprint(upload_bp)
+#     # Register the Blueprint
+#     app.register_blueprint(upload_bp)
 
-    return app
+#     return app
